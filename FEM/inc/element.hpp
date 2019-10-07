3d7fa67937f2c54809c997bf9bee4a0af10832c3
Máté Kelemen <kelemen.mate.12@gmail.com>
2019-10-07 11:22:36 +0200

    added fem project

---
+++ FEM/inc/element.hpp
@@ -0,0 +1,21 @@
+#ifndef FEM_ELEMENT_HPP
+#define FEM_ELEMENT_HPP
+
+#include "linalg.hpp"
+
+namespace fem {
+
+
+typedef cie::linalg::Matrix Matrix;
+
+
+class Element {
+public:
+	virtual Matrix& MaterialStiffness() const { return Matrix; }
+	virtual Matrix& GeometricStiffness() const { return Matrix; }
+};
+
+
+}
+
+#endif
