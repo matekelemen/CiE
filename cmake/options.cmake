d717dc0ade78d17ae74e3531043d2137b0345ef6
Mate Kelemen <ge49pil@mytum.de>
2020-02-07 14:31:27 +0100

    add glfw

---
+++ cmake/options.cmake
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------
+# GENERAL OPTIONS
+# ---------------------------------------------------------
+OPTION( CIE_INSTALL_TESTS "Install unit-and system tests" ON )
+
+# ---------------------------------------------------------
+# OPENGL OPTIONS
+# ---------------------------------------------------------
+OPTION( CIE_USE_CPP_GRAPHICS "Compile OpenGL and GLEW" ON )
---
+++ libraries/meshkernel/inc/mesh.hpp
@@ -0,0 +1,34 @@
+#ifndef MESHKERNEL_MESH_HPP
+#define MESHKERNEL_MESH_HPP
+
+#include "linalgtypes.hpp"
+#include <memory>
+
+namespace cie {
+namespace meshkernel {
+
+
+using VertexGrid        = std::array<linalg::Matrix, 3>;
+using TriangleVector    = std::vector<UIntArray<3>>;
+
+using VertexGridPtr     = std::unique_ptr<VertexGrid>;
+using TriangleVectorPtr = std::unique_ptr<TriangleVector>;
+
+
+class TriangleMesh 
+{
+public:
+    TriangleMesh() = delete;
+    explicit TriangleMesh(  VertexGrid& vertices,
+                            TriangleVector& faces );    
+
+private:
+    VertexGridPtr       _vertices;
+    TriangleVectorPtr   _faces;
+
+};
+
+}
+}
+
+#endif
