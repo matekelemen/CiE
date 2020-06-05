ceacce8fcf9c647e2ecf57455c32385e8abb2831
Mate Kelemen <mate.kelemen.12@gmail.com>
2020-06-05 20:18:22 +0200

    update submodules

--- libraries/FEM/python/modules/pyfem/optcontrol/adjointmodel.py
+++ libraries/FEM/python/modules/pyfem/optcontrol/adjointmodel.py
@@ -2,26 +2,39 @@
 from pyfem.discretization import NonlinearTransientFEModel
 
 # --- Python Imports ---
-
+import numpy as np
 
 # ---------------------------------------------------------
 class AdjointModel(NonlinearTransientFEModel):
     '''
     Nonlinear adjoint model attached to a NonlinearTransientFEModel
     '''
-    def __init__( self, model, time, forwardTimeHistory, *args, **kwargs ):
+    def __init__( self, model, time, referenceTimeHistory, forwardTimeHistory, *args, **kwargs ):
         NonlinearTransientFEModel.__init__( self, *args, **kwargs )
 
         # Check model type
         assert type(model) is NonlinearTransientFEModel, "Invalid model type!"
 
         self.femodel            = model
-        self.forwardSolution    = (time, forwardTimeHistory)
+        self.time               = time
+        self.referenceSolution  = referenceTimeHistory
+        self.forwardSolution    = forwardTimeHistory
+
+        self._loadFunction      = self.adjointLoadFunction
         
         # Transform model members
         for element in model.elements:
             self.elements.append(element.copy())
 
     
-    def makeForwardSampleFunction( self, timeIndex ):
-        return lambda x: self.femodel.sample( self.forwardSolution[1][timeIndex], x )
+    def makeSampleFunctor( self, coefficients ):
+        return lambda x: self.femodel.sample( coefficients, x )
+
+    
+    def adjointLoadFunction( self, t, x ):
+        timeIndex = next( (index for index,time in enumerate(self.time) if np.ans(time-t)>1e-16), -1 )
+        assert timeIndex > 0, "Time index not found for t=%.3f" % t
+
+        referenceField  = self.makeSampleFunctor( self.referenceSolution[timeIndex] )
+        forwardField    = self.makeSampleFunctor( self.forwardSolution[timeIndex] )
+        return lambda t,x: referenceField(x) - forwardField(x)
