28feca33861ab5cb4d7df6a221ab9a0bc971a339
Mate Kelemen <ge49pil@mytum.de>
2020-12-13 11:49:38 +0100

    add MarchingContainer

---
+++ libraries/cieutils/packages/stl_extension/impl/MarchingContainer_impl.hpp
@@ -0,0 +1,125 @@
+#ifndef CIE_UTILS_MARCHING_CONTAINER_IMPL_HPP
+#define CIE_UTILS_MARCHING_CONTAINER_IMPL_HPP
+
+// --- Internal Includes ---
+#include "cieutils/packages/macros/inc/checks.hpp"
+
+// --- STL Includes ---
+#include <algorithm>
+
+
+namespace cie::utils {
+
+
+template <class ValueType>
+template <class ContainerType>
+requires concepts::ClassContainer<ContainerType,ValueType>
+MarchingContainer<ValueType>::MarchingContainer( const ContainerType& r_initializer ) :
+    _capacity( r_initializer.size() ),
+    _counter( r_initializer.size() ),
+    _container( r_initializer.size() )
+{
+    std::copy( r_initializer.begin(),
+               r_initializer.end(),
+               this->_container.begin() );
+}
+
+
+template <class ValueType>
+MarchingContainer<ValueType>::MarchingContainer( Size capacity, const value_type& r_initializer ) :
+    _capacity( capacity ),
+    _counter( capacity ),
+    _container( capacity, r_initializer )
+{
+}
+
+
+template <class ValueType>
+MarchingContainer<ValueType>::MarchingContainer( Size capacity ) :
+    _capacity( capacity ),
+    _counter( 0 ),
+    _container( capacity )
+{
+}
+
+
+template <class ValueType>
+template <class ...Args>
+inline void
+MarchingContainer<ValueType>::emplace_back( Args&&... args )
+{
+    this->_counter++;
+    this->_container[ this->_counter % this->_capacity ] = std::move(
+        ValueType( std::forward<Args>(args)... )
+    );
+}
+
+
+template <class ValueType>
+inline void
+MarchingContainer<ValueType>::push_back( const ValueType& r_value )
+{
+    this->_counter++;
+    this->_container[ this->_counter % this->_capacity ] = r_value;
+}
+
+
+template <class ValueType>
+inline ValueType&
+MarchingContainer<ValueType>::back()
+{
+    return this->_container[ this->_counter % this->_capacity ];
+}
+
+
+template <class ValueType>
+inline const ValueType&
+MarchingContainer<ValueType>::back() const
+{
+    return this->_container[ this->_counter % this->_capacity ];
+}
+
+
+template <class ValueType>
+inline ValueType&
+MarchingContainer<ValueType>::operator[]( Size index )
+{
+    CIE_OUT_OF_RANGE_CHECK( this->outOfRangeCheck(index) )
+    return this->_container[ (index+1) % this->_capacity ];
+}
+
+
+template <class ValueType>
+inline const ValueType&
+MarchingContainer<ValueType>::operator[]( Size index ) const
+{
+    CIE_OUT_OF_RANGE_CHECK( this->outOfRangeCheck(index) )
+    return this->_container[ (index+1) % this->_capacity ];
+}
+
+
+template <class ValueType>
+inline Size
+MarchingContainer<ValueType>::size() const
+{
+    return this->_counter;
+}
+
+
+template <class ValueType>
+inline bool
+MarchingContainer<ValueType>::outOfRangeCheck( Size index )
+{
+    if ( this->_counter >= this->_capacity ) [[likely]]
+        return this->_counter - this->_capacity <= index
+               &&
+               index < this->_counter;
+    else
+        return index < this->_counter;
+}
+
+
+} // namespace cie::utils
+
+
+#endif
---
+++ libraries/cieutils/packages/stl_extension/test/MarchingContainer_test.cpp
@@ -0,0 +1,108 @@
+// --- External Includes ---
+#include "catch.hpp"
+
+// --- Utility Includes ---
+#include "cieutils/packages/macros/inc/testing.hpp"
+
+// --- Internal Includes ---
+#include "cieutils/packages/stl_extension/inc/MarchingContainer.hpp"
+
+
+namespace cie::utils {
+
+
+TEST_CASE( "MarchingContainer", "[stl_extension]" )
+{
+    CIE_TEST_CASE_INIT( "MarchingContainer" )
+
+    using ValueType     = int;
+    using ContainerType = MarchingContainer<ValueType>;
+
+    for ( Size capacity=3; capacity<7; ++capacity )
+    {
+        CIE_TEST_CASE_INIT( "capacity " + std::to_string(capacity) )
+
+        {
+            REQUIRE_NOTHROW( ContainerType(capacity) );
+            ContainerType container( capacity );
+
+            ValueType counter = 0;
+            CHECK_NOTHROW( container.push_back( ++counter ) );
+            CHECK_NOTHROW( container.emplace_back( ++counter ) );
+            
+            CHECK_NOTHROW( container.back() );
+            CHECK( container.back() == counter );
+
+            CHECK_NOTHROW( container.back() = ++counter );
+            CHECK( container.back() == counter );
+            auto tmp = container.back();
+
+            CHECK_NOTHROW( container[0] );
+            CHECK( container[0] == 1 );
+
+            CHECK_NOTHROW( container[0] = ++counter );
+            CHECK( container[0] == counter );
+
+            CHECK_NOTHROW( container[1] );
+            CHECK( container[1] == tmp );
+
+            CHECK_NOTHROW( container[1] = ++counter );
+            CHECK( container[1] == counter );
+
+            CHECK_NOTHROW( container.size() );
+            CHECK( container.size() == 2 );
+
+            CHECK_NOTHROW( container[ container.size() - 1 ] );
+            CHECK( container[ container.size()-1 ] == container.back() );
+
+            #ifdef CIE_ENABLE_OUT_OF_RANGE_TESTS
+            CHECK_THROWS( container[container.size()] );
+            #endif
+
+            for ( Size i=0; i<3*capacity; ++i )
+                CHECK_NOTHROW( container.push_back( ValueType(++counter) ) );
+
+            #ifdef CIE_ENABLE_OUT_OF_RANGE_TESTS
+            CHECK_THROWS( container[ container.size() - capacity - 1 ] );
+            #endif
+
+            for ( Size i=0; i<capacity; ++i )
+            {
+                Size index = container.size() - capacity + i;
+                REQUIRE_NOTHROW( container[index] );
+                CHECK( container[index] == ValueType(counter-capacity+i+1) );
+            }
+
+            CHECK( container.back() == counter );
+
+            #ifdef CIE_ENABLE_OUT_OF_RANGE_TESTS
+            CHECK_THROWS( container[container.size()] );
+            #endif
+        }
+
+        {
+            ValueType initializer = 12;
+            ContainerType container( capacity, initializer );
+
+            CHECK( container.size() == capacity );
+            
+            for ( Size i=0; i<capacity; ++i )
+            {
+                CHECK_NOTHROW( container[i] );
+                CHECK( container[i] == initializer );
+            }
+
+            for ( Size i=capacity; i<4*capacity+1; ++i )
+                CHECK_NOTHROW( container.emplace_back( ValueType(i) ) );
+
+            for ( Size i=container.size()-capacity; i<container.size(); ++i )
+            {
+                CHECK_NOTHROW( container[i] );
+                CHECK( container[i] == ValueType(i) );
+            }
+        }
+    }
+}
+
+
+} // namespace cie::utils
