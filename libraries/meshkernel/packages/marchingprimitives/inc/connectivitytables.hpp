2645fa6418be83cb237118147f535f965e721619
Máté Kelemen <ge49pil@mytum.de>
2021-01-03 01:49:29 +0100

    add maps for marching squares

--- libraries/meshkernel/packages/marchingprimitives/inc/connectivitytables.hpp
+++ libraries/meshkernel/packages/marchingprimitives/inc/connectivitytables.hpp
@@ -11,15 +11,38 @@
 namespace cie::mesh::detail {
 
 
+const std::vector<std::pair<Size,Size>> squareEdgeMap
+{
+	{0, 1}, {1,3}, {3,2}, {2,0}
+};
+
+
 const std::vector<std::pair<Size,Size>> cubeEdgeMap
 {
-    //{0,1}, {1,2}, {2,3}, {3,0},
-    //{4,5}, {5,6}, {6,7}, {7,4},
-    //{0,4}, {1,5}, {2,6}, {3,7}
-
     {0,1}, {1,3}, {3,2}, {2,0},
     {4,5}, {5,7}, {7,6}, {6,4},
     {0,4}, {1,5}, {3,7}, {2,6}
+};
+
+
+const std::vector<std::vector<std::vector<Size>>> marchingSquaresConnectivityMap
+{
+	{},
+	{ {0,3} },
+	{ {0,1} },
+	{ {1,3} },
+	{ {3,2} },
+	{ {0,2} },
+	{ {0,1}, {3,2} },
+	{ {1,2} },
+	{ {1,2} },
+	{ {1,2}, {3,0} },
+	{ {0,2} },
+	{ {2,3} },
+	{ {1,3} },
+	{ {0,1} },
+	{ {0,3} },
+	{}
 };
 
 
