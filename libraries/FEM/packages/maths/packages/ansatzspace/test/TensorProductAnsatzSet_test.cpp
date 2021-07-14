9ba584930b96e467758cfe51c3cc1fce9128d1ef
Mate Kelemen <ge49pil@mytum.de>
2021-07-14 18:47:05 +0200

    reorder test checks

--- libraries/FEM/packages/maths/packages/ansatzspace/test/TensorProductAnsatzSet_test.cpp
+++ libraries/FEM/packages/maths/packages/ansatzspace/test/TensorProductAnsatzSet_test.cpp
@@ -41,40 +41,40 @@ CIE_TEST_CASE( "TensorProductAnsatzSpace", "[maths]" )
     CIE_TEST_CHECK( ansatzSet[0](p2) == Approx(-25.0) );
     CIE_TEST_CHECK( ansatzSet[0](p3) == Approx(20.0) );
 
-    CIE_TEST_CHECK( ansatzSet[3](p0) == Approx(-24.0) );
-    CIE_TEST_CHECK( ansatzSet[3](p1) == Approx(30.0) );
-    CIE_TEST_CHECK( ansatzSet[3](p2) == Approx(20.0) );
-    CIE_TEST_CHECK( ansatzSet[3](p3) == Approx(-25.0) );
-
-    CIE_TEST_CHECK( ansatzSet[6](p0) == Approx(486.0) );
-    CIE_TEST_CHECK( ansatzSet[6](p1) == Approx(486.0) );
-    CIE_TEST_CHECK( ansatzSet[6](p2) == Approx(-405.0) );
-    CIE_TEST_CHECK( ansatzSet[6](p3) == Approx(-405.0) );
-
     CIE_TEST_CHECK( ansatzSet[1](p0) == Approx(-25.0) );
     CIE_TEST_CHECK( ansatzSet[1](p1) == Approx(20.0) );
     CIE_TEST_CHECK( ansatzSet[1](p2) == Approx(30.0) );
     CIE_TEST_CHECK( ansatzSet[1](p3) == Approx(-24.0) );
-
-    CIE_TEST_CHECK( ansatzSet[4](p0) == Approx(20.0) );
-    CIE_TEST_CHECK( ansatzSet[4](p1) == Approx(-25.0) );
-    CIE_TEST_CHECK( ansatzSet[4](p2) == Approx(-24.0) );
-    CIE_TEST_CHECK( ansatzSet[4](p3) == Approx(30.0) );
-
-    CIE_TEST_CHECK( ansatzSet[7](p0) == Approx(-405.0) );
-    CIE_TEST_CHECK( ansatzSet[7](p1) == Approx(-405.0) );
-    CIE_TEST_CHECK( ansatzSet[7](p2) == Approx(486.0) );
-    CIE_TEST_CHECK( ansatzSet[7](p3) == Approx(486.0) );
 
     CIE_TEST_CHECK( ansatzSet[2](p0) == Approx(605.0) );
     CIE_TEST_CHECK( ansatzSet[2](p1) == Approx(-484.0) );
     CIE_TEST_CHECK( ansatzSet[2](p2) == Approx(605.0) );
     CIE_TEST_CHECK( ansatzSet[2](p3) == Approx(-484.0) );
 
+    CIE_TEST_CHECK( ansatzSet[3](p0) == Approx(-24.0) );
+    CIE_TEST_CHECK( ansatzSet[3](p1) == Approx(30.0) );
+    CIE_TEST_CHECK( ansatzSet[3](p2) == Approx(20.0) );
+    CIE_TEST_CHECK( ansatzSet[3](p3) == Approx(-25.0) );
+
+    CIE_TEST_CHECK( ansatzSet[4](p0) == Approx(20.0) );
+    CIE_TEST_CHECK( ansatzSet[4](p1) == Approx(-25.0) );
+    CIE_TEST_CHECK( ansatzSet[4](p2) == Approx(-24.0) );
+    CIE_TEST_CHECK( ansatzSet[4](p3) == Approx(30.0) );
+
     CIE_TEST_CHECK( ansatzSet[5](p0) == Approx(-484.0) );
     CIE_TEST_CHECK( ansatzSet[5](p1) == Approx(605.0) );
     CIE_TEST_CHECK( ansatzSet[5](p2) == Approx(-484.0) );
     CIE_TEST_CHECK( ansatzSet[5](p3) == Approx(605.0) );
+
+    CIE_TEST_CHECK( ansatzSet[6](p0) == Approx(486.0) );
+    CIE_TEST_CHECK( ansatzSet[6](p1) == Approx(486.0) );
+    CIE_TEST_CHECK( ansatzSet[6](p2) == Approx(-405.0) );
+    CIE_TEST_CHECK( ansatzSet[6](p3) == Approx(-405.0) );
+
+    CIE_TEST_CHECK( ansatzSet[7](p0) == Approx(-405.0) );
+    CIE_TEST_CHECK( ansatzSet[7](p1) == Approx(-405.0) );
+    CIE_TEST_CHECK( ansatzSet[7](p2) == Approx(486.0) );
+    CIE_TEST_CHECK( ansatzSet[7](p3) == Approx(486.0) );
 
     CIE_TEST_CHECK( ansatzSet[8](p0) == Approx(9801.0) );
     CIE_TEST_CHECK( ansatzSet[8](p1) == Approx(9801.0) );
